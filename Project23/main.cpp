d4cd2ba729211f283b59ecec8b24f7c5d26e7f3b
xmmt <mkmax472@gmail.com>
2021-02-01 02:33:43 +0300

    testing

--- Project23/main.cpp
+++ Project23/main.cpp
@@ -18,13 +18,46 @@ public:
 };
 
 int main() {
+    using namespace std::chrono_literals;
     std::atomic_int counter{ 0 };
-    threading::threadpool tp(80);
-    for (int i = 0; i < 120; ++i) {
+    threading::threadpool tp(20);
+
+    std::packaged_task<int()> task{ [] {
+        std::this_thread::sleep_for(5s);
+        return 5;
+    } };
+    std::future<int> result = task.get_future();
+    tp.submit(task);
+
+    for (int i = 0; i < 5; ++i) {
         tp.submit([i, &counter] {
-            for (int j = 0; j < 1000; ++j) {
-                Writer() << i << ' ' << j << '\n';
+            for (int j = 0; j < 8; ++j) {
+                Writer() << i << (i < 10 ? " " : "") << ' ' << j << '\n';
                 counter++;
+                std::this_thread::sleep_for(200ms);
+            }
+        });
+    }
+    Writer() << result.get() << '\n';
+    std::this_thread::sleep_for(10s);
+    Writer() << counter << '\n';
+    for (int i = 0; i < 40; ++i) {
+        tp.submit([i, &counter] {
+            for (int j = 0; j < 8; ++j) {
+                Writer() << i << (i < 10 ? " " : "") << ' ' << j << '\n';
+                counter++;
+                std::this_thread::sleep_for(200ms);
+            }
+        });
+    }
+    std::this_thread::sleep_for(10s);
+    Writer() << counter << '\n';
+    for (int i = 0; i < 40; ++i) {
+        tp.submit([i, &counter] {
+            for (int j = 0; j < 8; ++j) {
+                Writer() << i << (i < 10 ? " " : "") << ' ' << j << '\n';
+                counter++;
+                std::this_thread::sleep_for(200ms);
             }
         });
     }
